5b81c99972fed12ad23536497ab386bdf8e296d7
agrasagar <agrasagar@gmail.com>
2015-11-13 03:18:11 +0200

    packet package extended and GUI basic view added

--- src/packet/packet.cc
+++ src/packet/packet.cc
@@ -1,24 +1,54 @@
 #include "packet.h"
 #include <iostream>
-
+#include "packet_interface.h"
 packet::packet(){
-  this->source = 1; //first node
-  this->destination = 10; //last node
+  this->sourceNode = 1; //first node
+  this->destinationNode = 10; //last node
   this->maxHopCount = 10;
 }
 
-packet::packet(int source,int destination){
-  this->source = source;
-  this->destination = destination;
+packet::packet(int sourceNode,int destination){
+  this->sourceNode = sourceNode;
+  this->destinationNode = destinationNode;
   this->maxHopCount = 10;
+  this->packet_id = "AAA";
+  this->packet_size = 100;
 }
 
-packet::packet(int source, int destination, int maxHopCount){
-  this->source = source;
-  this->destination = destination;
+packet::packet(int sourceNode, int destinationNode, int maxHopCount){
+  this->sourceNode = sourceNode;
+  this->destinationNode = destinationNode;
   this->maxHopCount = maxHopCount;
+  this->packet_id = "AAA1";
+  this->packet_size = 100;
+}
+packet::packet(int sourceNode,int destinationNode,int maxHopCount, std::string packet_id, int packet_size){
+  this->sourceNode = sourceNode;
+  this->destinationNode = destinationNode;
+  this->maxHopCount = maxHopCount;
+  this->packet_id = packet_id;
+  this->packet_size = packet_size;
+}
+
+std::string packet::getPacket_id(){
+  return this->packet_id;
+}
+int packet::getDestintionNode(){
+  return this->destinationNode;
+}
+
+int packet::getSourceNode(){
+  return this->sourceNode;
+}
+
+int packet::getMaxHopCount(){
+  return this->maxHopCount;
+}
+
+int packet::getPacket_size(){
+  return packet_size;
 }
 //test function
 int packet::showDetails(){
-  std::cout <<this->destination<< this->maxHopCount<< this->source<< std::endl;
+  std::cout <<getPacket_id()<<getPacket_size()<< getSourceNode()<< getDestintionNode()<< std::endl;
 }
--- src/packet/packet.h
+++ src/packet/packet.h
@@ -1,20 +1,32 @@
 #ifndef _PACKET_H_
 #define _PACKET_H_
 
+#include <string>
 
 class packet
 {
 
 public:
   packet();
-  packet(int source, int destination);
-  packet(int source,int destination,int maxHopCount);
+  packet(int sourceNode, int destinationNode);
+  packet(int sourceNode,int destinationNode,int maxHopCount);
+  //test constructor with packet id and size
+  packet(int sourceNode,int destination,int maxHopCount, std::string packet_id, int packet_size);
+  int getSourceNode();
+  int getDestintionNode();
+  int getMaxHopCount();
+  int getPacket_size();
+  //to be made random and added function to check unique random
+  std::string getPacket_id();
+  //test function
   int showDetails();
 
 private:
-  int source;
-  int destination;
+  int sourceNode;
+  int destinationNode;
   int maxHopCount;
+  std::string packet_id;
+  int packet_size;
 
 };
 
