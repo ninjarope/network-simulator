286c11cb54be029db684b791335e83d9362626cc
Tommi Grohn <grohn.tommi@gmail.com>
2015-12-03 10:08:44 +0200

    Some improvements to layout

--- src/main.cpp
+++ src/main.cpp
@@ -27,33 +27,40 @@ int main() {
     ns.addNode("B");
     ns.addNode("C");
     ns.addNode("D");
+    ns.addNode("E");
 
     // add some applications running on nodes
     ns.getNode("A")->addApplications(new PacketReceiver);
     ns.getNode("A")->addApplications(new PacketGenerator(10, ns.getAddresses()));
-    ns.getNode("A")->addApplications(new RandomRouter);
+    ns.getNode("A")->addApplications(new TestRouter);
     ns.getNode("B")->addApplications(new PacketReceiver);
     ns.getNode("B")->addApplications(new TestRouter);
     ns.getNode("C")->addApplications(new PacketReceiver);
+    ns.getNode("C")->addApplications(new PacketGenerator(10, ns.getAddresses()));
     ns.getNode("C")->addApplications(new TestRouter);
     ns.getNode("D")->addApplications(new PacketReceiver);
     ns.getNode("D")->addApplications(new TestRouter);
+    ns.getNode("E")->addApplications(new PacketReceiver);
+    ns.getNode("E")->addApplications(new TestRouter);
     
     // create some links between nodes
-    ns.addLink("A", "C", new ParametricLink(4.0, 16.0, 4.0));
-    ns.addLink("C", "A", new ParametricLink(4.0, 16.0, 4.0));
-
     ns.addLink("A", "B", new ParametricLink(8.0, 16.0, 4.0));
     ns.addLink("B", "A", new ParametricLink(8.0, 16.0, 4.0));
     
-    ns.addLink("B", "C", new ParametricLink(4.0, 16.0));
-    ns.addLink("C", "B", new ParametricLink(4.0, 16.0));
+    ns.addLink("B", "C", new ParametricLink(1.0, 16.0));
+    ns.addLink("C", "B", new ParametricLink(1.0, 16.0));
 
     ns.addLink("C", "D", new ParametricLink(4.0, 16.0));
     ns.addLink("D", "C", new ParametricLink(4.0, 16.0));
     
     ns.addLink("D", "A", new ParametricLink(2.0, 16.0));
     ns.addLink("A", "D", new ParametricLink(2.0, 16.0));
+
+    ns.addLink("D", "E", new ParametricLink(2.0, 16.0));
+    ns.addLink("E", "D", new ParametricLink(2.0, 16.0));
+
+    ns.addLink("B", "E", new ParametricLink(2.0, 16.0));
+    ns.addLink("E", "B", new ParametricLink(2.0, 16.0));
 
     // Update routing tables
     ns.update();
