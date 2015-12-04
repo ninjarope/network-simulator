02be5a802631ab79f544aad5ab90760c94958fb6
Joni Turunen <rojun.itu@gmail.com>
2015-12-04 12:08:33 +0200

    Test framework assembled and few tests written (xml loading, network adding nodes)

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,10 @@
 //
 //  Created by Tommi Gröhn on 13.11.2015.
 //
+
+#define CATCH_CONFIG_RUNNER
+#include "test/catch.h"
+
 #include <iostream>
 #include <string>
 
@@ -16,45 +20,12 @@
 #include "application/PacketReceiver.h"
 #include "application/TestRouter.h"
 
+
 // dependency for xml parsing
 #include "xml/tinyxml2.h"
 #include "xml/XMLReader.h"
 
-/**
- * Simple test case.
- */
-void test1(NetworkSimulator& ns, NetworkSimulatorUI* ui);
-void runUI(NetworkSimulator& ns, NetworkSimulatorUI* ui);
-
-/**
- * test case for loading xml
- *
- * @filepath relative filepath to xml file
- */
-void testXMLFileLoad(const char* filepath) {
-    tinyxml2::XMLDocument doc;
-    doc.LoadFile(filepath);
-    switch (doc.ErrorID()) {
-        case 0:
-            std::cout << "OK. No problem! XML File loaded." << std::endl;
-            break;
-        default:
-            std::cout << "XML file loading error! Error code: "
-                << doc.ErrorID()
-                << std::endl;
-    }
-}
-
-void testNodesInGui(NetworkSimulator& ns, NetworkSimulatorUI* ui) {
-    // create some nodes
-    ns.addNode("A");
-    ns.addNode("B");
-    ns.addNode("C");
-    ns.addNode("D");
-    ns.addNode("E");
-
-    runUI(ns, ui);
-}
+#include "test/Tests.h"
 
 void runUI(NetworkSimulator& ns, NetworkSimulatorUI* ui) {
     // Update routing tables
@@ -79,73 +50,22 @@ void testXMLReader(NetworkSimulator& ns, NetworkSimulatorUI* ui) {
         std::cerr << msg << std::endl;
     }
 }
+
 int main(int argc, char** argv) {
     NetworkSimulator ns;
     NetworkSimulatorUI* ui = NetworkSimulatorGUI::createUI();
     ns.setUI(ui);
 
-    testXMLReader(ns, ui);
+    int result = 0;
+
+    // Arguments override behaviour -> testing
+    if (argc < 2) {
+        testXMLReader(ns, ui);
 //    testNodesInGui(ns, ui);
 //    testXMLFileLoad("resources/in.xml");
 //    test1(ns, ui);
-}
+    } else
+        result = Catch::Session().run(argc, argv);
 
-
-void test1(NetworkSimulator& ns, NetworkSimulatorUI* ui) {
-    // TODO Clear the out commented parts, that aren't necessary..
-
-    // create some nodes
-    ns.addNode("A");
-    ns.addNode("B");
-    ns.addNode("C");
-    ns.addNode("D");
-    ns.addNode("E");
-
-    // add some applications running on nodes
-    ns.getNode("A")->addApplications(new PacketReceiver);
-    ns.getNode("A")->addApplications(new PacketGenerator(1000, ns.getAddresses()));
-    ns.getNode("A")->addApplications(new TestRouter);
-    ns.getNode("B")->addApplications(new PacketReceiver);
-    ns.getNode("B")->addApplications(new TestRouter);
-    ns.getNode("C")->addApplications(new PacketReceiver);
-    ns.getNode("C")->addApplications(new PacketGenerator(800, ns.getAddresses()));
-    ns.getNode("C")->addApplications(new TestRouter);
-    ns.getNode("D")->addApplications(new PacketReceiver);
-    ns.getNode("D")->addApplications(new TestRouter);
-    ns.getNode("E")->addApplications(new PacketReceiver);
-    ns.getNode("E")->addApplications(new TestRouter);
-
-    // create some links between nodes
-    ns.addLink("A", "B", new ParametricLink(1.0, 160.0, 4.0));
-    ns.addLink("B", "A", new ParametricLink(1.0, 160.0, 4.0));
-    
-    ns.addLink("B", "C", new ParametricLink(1.0, 16.0));
-    ns.addLink("C", "B", new ParametricLink(1.0, 16.0));
-
-    ns.addLink("C", "D", new ParametricLink(1.0, 160.0));
-    ns.addLink("D", "C", new ParametricLink(1.0, 160.0));
-    
-    ns.addLink("D", "A", new ParametricLink(2.0, 16.0));
-    ns.addLink("A", "D", new ParametricLink(2.0, 16.0));
-
-    ns.addLink("A", "E", new ParametricLink(1.0, 160.0));
-    ns.addLink("E", "A", new ParametricLink(1.0, 160.0));
-    
-    ns.addLink("B", "E", new ParametricLink(2.0, 16.0));
-    ns.addLink("E", "B", new ParametricLink(2.0, 16.0));
-
-    ns.addLink("D", "E", new ParametricLink(2.0, 16.0));
-    ns.addLink("E", "D", new ParametricLink(2.0, 16.0));
-
-
-    // Update routing tables
-    ns.updateRouting();
-   
-    // run (timer has currently some hard-coded test values)
-
-    ui->generateGraphLayout();
-    ns.start();
-
-    // print traffic log for given node (packetID's and time stamps)
-    //ui->displayTrafficLog("A", "B");
+    return result;
 }
