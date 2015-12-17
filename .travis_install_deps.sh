dd2c600c079c852bba5cd6441c7f21d27685b9ad
Joni Turunen <rojun.itu@gmail.com>
2015-12-17 09:15:22 +0200

    TravisCI: install deps with script

---
+++ .travis_install_deps.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+sudo apt-get update -qq
+if [ "$CXX" = "g++" ]; then sudo apt-get install -qq g++-4.8; fi
+if [ "$CXX" = "g++" ]; then export CXX="g++-4.8" CC="gcc-4.8"; fi
+
+echo $CXX
+g++ --version
+
+sudo apt-get install -y libsfml-dev
+
